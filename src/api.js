75cdf8f212d3420aea0d824ee6f70d85c5635ca5
yair <yairmasterlol@gmail.com>
2023-07-20 16:55:38 -0600

    2023-07-20-04:55:38 PM

--- src/api.js
+++ src/api.js
@@ -1,2 +1,2 @@
-export const api = "http://127.0.0.1:8000/api";
-// export const api = "https://irais-production.up.railway.app/api";
+// export const api = "http://127.0.0.1:8000/api";
+export const api = "https://irais-production.up.railway.app/api";
